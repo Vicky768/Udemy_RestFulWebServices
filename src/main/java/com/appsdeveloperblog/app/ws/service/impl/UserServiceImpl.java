f32fdbad1d4c6447fbe327280571f14572648f09
unknown <roysudeepto168@gmail.com>
2021-10-15 01:57:26 +0530

    First days work

---
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.appsdeveloperblog.app.ws.service.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.appsdeveloperblog.app.ws.UserRepository;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utils;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	Utils utils;
+	
+	@Override
+	public UserDto createUser(UserDto user) {
+						
+		if(userRepository.findByEmail(user.getEmail()) != null)
+			throw new RuntimeException("Record already exists");
+		
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+		
+		String publicUserId = utils.generatedUserId(30);
+		userEntity.setUserId(publicUserId);
+		userEntity.setEncryptedPassword("test");
+		
+		UserEntity storedUserDetails = userRepository.save(userEntity);
+				
+		UserDto returnValue = new UserDto();
+		BeanUtils.copyProperties(storedUserDetails, returnValue);
+		return returnValue;
+	}
+	
+	
+}
